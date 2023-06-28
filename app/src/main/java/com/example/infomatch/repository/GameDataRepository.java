6abb0c25ee9be010642b3835354a5b007c5b735b
YUVAL-K <yuvalikit18@gmail.com>
2023-06-28 19:46:14 +0300

    Changes

---
+++ app/src/main/java/com/example/infomatch/repository/GameDataRepository.java
@@ -0,0 +1,26 @@
+package com.example.infomatch.repository;
+
+import com.example.infomatch.data.GameResult;
+
+import java.util.List;
+
+public class GameDataRepository {
+
+
+//    fun getExercises(workoutId: String) = exerciseDao?.getExercises(workoutId)
+//
+//    suspend fun addExercise(exercise_item: Exercise_Item) {
+//        exerciseDao?.addExercise(exercise_item)
+//    }
+//
+//    suspend fun deleteExercise(exercise_item: Exercise_Item) {
+//        exerciseDao?.deleteExercise(exercise_item)
+//    }
+//
+//    fun getItem(id: Int) = exerciseDao?.getExercise(id)
+
+    public List<GameResult> getAll() {
+        return
+    }
+
+}
